1733071c2376ca55601b3ed71e5b7a5abc7a3594
Youssef Laamarti <88171482+YoussefLaamarti@users.noreply.github.com>
2023-01-04 02:20:59 +0100

    Update requests.jsx

--- src/requests.jsx
+++ src/requests.jsx
@@ -1,4 +1,4 @@
-const APIKEY = $API_KEY;
+const APIKEY = "";
 const requests = {
   fetchPopularMovies: `/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}`,
   fetchHighestRating: `/discover/movie/?certification_country=US&certification=R&api_key=${APIKEY}`,
